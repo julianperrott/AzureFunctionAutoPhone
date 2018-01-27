beab9202965348576b15e199cfd416ea653c8422
Julian Perrott <julian_perrott@hotmail.com>
2018-01-27 13:46:11 +0000

    Inital Project

--- AzureFunctionAutoPhone/StartCall.cs
+++ AzureFunctionAutoPhone/StartCall.cs
@@ -14,16 +14,7 @@ namespace AzureFunctionAutoPhone
         public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return (ActionResult)new OkObjectResult($"Hello, World");
         }
     }
 }
