18273f7b3eb6751405310ae7f61589979c2ab85c
Julian Perrott <julian_perrott@hotmail.com>
2018-01-28 08:13:46 +0000

    Fix

--- AzureFunctionAutoPhone/StartCall.cs
+++ AzureFunctionAutoPhone/StartCall.cs
@@ -16,11 +16,11 @@ namespace AzureFunctionAutoPhone
         {
             log.Info("Running StartCall");
 
-            TwilioClient.Init("AC12e83e21706054c0a1a997112f44fb84", "41f833750330c488cff9bdb24f8995fb"); // Twilio accountSid / authToken
+            TwilioClient.Init("", ""); // Twilio accountSid / authToken
 
             var call = CallResource.Create(to: new PhoneNumber("+447796758877"), // customer phone
                 from: "+441616948114", // Twilio managed number
-                applicationSid: "AP6b5649b78dd87176b849bb993fbf069e"); // Twilio -> Tools-> TwiML Apps -> Properties SID
+                applicationSid: ""); // Twilio -> Tools-> TwiML Apps -> Properties SID
 
             log.Info($"Call started: {call.Sid}");
             return (ActionResult)new OkObjectResult($"Call started: {call.Sid}");
